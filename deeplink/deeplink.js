302443b905741512500b48b58f2077ce65251785
sakamoto <mossan@fout.jp>
2016-08-05 12:33:40 +0900

    try-and-fallback.html

--- deeplink/deeplink.js
+++ deeplink/deeplink.js
@@ -1,3 +1,28 @@
-(function() {
+var Deeplink = (function() {
+  var Deeplink = function(params) {
+    this.appDeeplinkURL = params.appDeeplinkURL;
+    this.webFallbackURL = params.webFallbackURL;
+    this.fallbackDelay = params.fallbackDelay;
+  };
 
+  Deeplink.prototype.open = function () {
+    var iframe = document.createElement("iframe");
+
+    var fallbackTimeout = setTimeout(function(){
+      iframe.parentNode.removeChild(iframe);
+      window.open(this.webFallbackURL, '_blank');
+    }, this.fallbackDelay);
+
+    iframe.onload = function() {
+      clearTimeout(fallbackTimeout);
+      iframe.parentNode.removeChild(iframe);
+      window.location.href = this.appDeeplinkURL;
+    };
+
+    iframe.src = this.appDeeplinkURL;
+    iframe.setAttribute("style", "display:none;");
+    document.body.appendChild(iframe);
+  };
+
+  return Deeplink;
 })();
